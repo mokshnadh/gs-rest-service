71013f5cb9fbf9b80ea99a5c86fa5f0e3c18fb30
mokshnadh <mokshay@teamawsome.ltd>
2018-05-05 17:16:57 +0100

    trail

---
+++ Documents/workspace-sts-3.9.0.RELEASE/gs-rest-service/src/main/java/com/mycompany/product/ProductService.java
@@ -0,0 +1,25 @@
+package com.mycompany.product;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mycompany.product.Product;
+
+@RestController
+public class ProductService {
+
+	@RequestMapping("/{env}product/{id}")
+	Product getProduct(@PathVariable("id") int id) {
+		return new Product(id);
+	}
+
+	@RequestMapping("/productIds")
+	List<Integer> getProductIds(@RequestParam("id") int id) {
+		return Arrays.asList(id + 1, id + 2, id + 3);
+	}
+}
---
+++ Documents/workspace-sts-3.9.0.RELEASE/gs-rest-service/src/main/java/com/mycompany/product/ProductSpringApp.java
@@ -0,0 +1,15 @@
+package com.mycompany.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+
+public class ProductSpringApp {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ProductSpringApp.class, args);
+	}
+}
